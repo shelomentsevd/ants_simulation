26813bdfc62ce1a473ad83bdf77e9a1216d97d4c
Shelomentcev Dmitriy <sedecim13@gmail.com>
2015-08-12 23:05:40 +0300

    Random actor place, light

--- game.js
+++ game.js
@@ -11,18 +11,25 @@ var Actor = function (x, y) {
     this._x = x;
     this._y = y;
     this._draw();
+    this._lookAround();
+    //Delete x,y cell from freeCells
 };
 
 Actor.prototype._draw = function () {
     Game.display.draw(this._x, this._y, GVar.actor, "#ff0");
 };
 
+//Return's free cells in radius 1
+Actor.prototype._lookAround = function () {
+    Game.map.calculateView(this._x, this._y, 1);
+};
+
 //Map
 var Map = function (width, height) {
     this._width      = width;
     this._height     = height;
-    this._tiles      = {}; //All tiles
-    this._emptyTiles = []; //Tiles without actors and walls
+    this._cells      = {}; //All tiles
+    this._freeCells = []; //Tiles without actors and walls
     this._generate();
     this._draw();
 };
@@ -38,37 +45,75 @@ Map.prototype._generate = function () {
             tile = GVar.wall;
         } else {
             tile = GVar.space;
-            this._emptyTiles.push(key);
+            this._freeCells.push(key);
         }
         
-        this._tiles[key] = tile;
+        this._cells[key] = tile;
     };
     //generation starts here
     var arena = new ROT.Map.IceyMaze(this._width, this._height);
     arena.create(mapCallback.bind(this));
 };
+
 //Draw map
 Map.prototype._draw = function () {
-    for(var key in this._tiles) {
+    for(var key in this._cells) {
         //Just for fun ^_^
         var [x, y] = key.split(',').map(function(item) {
             return parseInt(item);
         });
-    Game.display.draw(x, y, this._tiles[key]);
+    Game.display.draw(x, y, this._cells[key]);
     }
 };
 
+Map.prototype.calculateView = function (x, y, r) {
+    
+    var visibilityCallback  = function (x, y) {
+        var key = [x, y].join(',');
+        return this._freeCells.indexOf(key) != -1;
+    };
+    
+    var fov = new ROT.FOV.DiscreteShadowcasting(visibilityCallback.bind(this));
+    var result = [];
+    var viewCallback = function (x, y, r, visible) {
+        if(visible) {
+            var key = [x, y].join(',');
+            this.push();
+            var tile = Game.map._cells[key];
+            Game.display.draw(x, y, tile, '#ff0');
+        }
+    };
+    
+    fov.compute(x, y, r, viewCallback.bind(result));
+    
+    return result;
+};
+
+Map.prototype.getRandomEmptyCell = function () {
+    var index = Math.floor(ROT.RNG.getUniform() * this._freeCells.length);
+    var key = this._freeCells[index];
+    return key.split(',').map(function(item) {
+            return parseInt(item);
+    });
+};
 //Game
 var Game = {
     display: null,
     
     options: {},
     
+    map: null,
+    
+    actors: [],
+    
     init: function() {
         this.display = new ROT.Display(this.options);
         document.body.appendChild(this.display.getContainer());
         this.options = this.display.getOptions();
-        var map = new Map(this.options.width, this.options.height);
+        this.map = new Map(this.options.width, this.options.height);
+        var [x, y] = this.map.getRandomEmptyCell();
+        console.log("X: %d Y: %d", x, y);
+        var player = new Actor(x, y);
     }
 }
 
